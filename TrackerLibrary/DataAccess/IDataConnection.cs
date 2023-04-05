78a724fbdb666e167e088916e05b52e714663c2d
Daniel <105551522+BananPirat@users.noreply.github.com>
2023-04-05 05:16:38 +0300

    Update IDataConnection.cs

--- TrackerLibrary/DataAccess/IDataConnection.cs
+++ TrackerLibrary/DataAccess/IDataConnection.cs
@@ -9,8 +9,10 @@ namespace TournamentTracker
         void CreatePerson(PersonModel model);
         void CreateTeam(TeamModel model);
         void CreateTournament(TournamentModel model);
+        
         void UpdateMatchup(MatchupModel model);
         void CompleteTournament(TournamentModel model);
+        
         List<TournamentModel> GetTournament_All();
         List<TeamModel> GetTeam_All();
         List<PersonModel> GetPerson_All();
